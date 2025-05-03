f2dbf1d9d469570d05433001231409da643cf2c3
vladeg01 <150160142+vladeg01@users.noreply.github.com>
2025-05-03 14:07:14 +0300

    last check

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import {Asteroids} from "./pages/Asteroids";
 import {Destroyment} from "./pages/Destroyment";
 import {Asteroid} from "./pages/Asteroid";
 import {AsteroidsContextProvider} from "./components/asteroids-context/AsteroidsContext";
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter, } from "react-router";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
     {
         path: "/asteroids",
         element: <Asteroids/>,
